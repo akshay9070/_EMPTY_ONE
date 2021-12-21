1f72eb8ff8e49e94dc6a1c91d4520c470288fd6e
ABHAY TARAS <73850631+abhay-h@users.noreply.github.com>
2021-12-22 00:49:11 +0530

    Add files via upload

---
+++ U_can_Edit.py
@@ -0,0 +1,4 @@
+str1="You can "
+print ("String 1:",str1)
+str1=str1*3
+print("XD:",str1)
---
+++ UcanEdit.java
@@ -0,0 +1,5 @@
+public class  UcanEdit  {
+  public static void main(String[] args) {
+    System.out.println("lol");
+  }
+}
